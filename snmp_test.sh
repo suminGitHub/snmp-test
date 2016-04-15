618913cde8829579594b71c4f386395d7442f411
Allard-Chris <lyon.allard.chris@gmail.com>
2016-04-15 16:51:27 +0200

    Added files via upload

---
+++ snmp_test.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+########################
+#                      #
+# SCRIPT SNMP SUR HOST #
+#        V1.0          #
+#Auteur : Barreyre.M   #
+#Auteur : Allard.C     #
+#                      #
+########################
+
+#  Table of cummunities
+tab_communitie=( "public" "prive" );
+
+# Table of versions
+tab_snmp=( "v1" "v2c" );
+
+# Output variable
+result="";
+
+# Loop on the hosts in the file hosts.txt
+for host in $(cat hosts.txt):
+do
+        echo "$host in progress";
+        result="Host : $host - ";
+        # Loop on communitie
+        for communitie in ${tab_communitie[*]};
+        do
+                # Loop on snmp's versions
+                for version in ${tab_snmp[*]};
+                do
+                        result+="[${communitie} ${version}] ";
+
+                        if [ -n "$1" ]
+                                then commande=$(snmpwalk -$version -c $communitie $host $1 2> /dev/null);
+                                else commande=$(snmpwalk -$version -c $communitie $host 2> /dev/null);
+                        fi;
+
+                        if [ -z "$commande" ]
+                                then result+=": No response | ";
+                                else result+=": Ok | ";
+                        fi;
+                done;
+        done;
+        echo "$host done";
+        echo $result >> result.txt;
+done;
